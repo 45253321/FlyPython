1237defb5bb397cc4f02421835a101b877146933
李自泰 <174158973@qq.com>
2015-11-23 07:24:16 +0800

    提交

---
+++ MovieScrapy/MovieScrapy/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for MovieScrapy project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'MovieScrapy'
+
+SPIDER_MODULES = ['MovieScrapy.spiders']
+NEWSPIDER_MODULE = 'MovieScrapy.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'MovieScrapy (+http://www.yourdomain.com)'
---
+++ MovieScrapy/MovieScrapy/spiders/MovieSpider.py
@@ -0,0 +1,29 @@
+#encoding=utf-8
+
+__author__ = 'lizitai'
+import scrapy
+from MovieScrapy.items import  MoviesItem
+from scrapy import log
+
+
+class MovieSpider(scrapy.Spider):
+    name = "movie"
+    allowed_domains = ["10.10.10.10"]
+    start_urls = [
+        "http://10.10.10.10/4/mvlist_tag_mv.aspx?typeID=0&areaID=15&timeID=0&sortID=0&starLevel=0",
+
+    ]
+
+    def parse(self, response):
+        movieList = response.xpath('//div[contains(@class, "movieList")]/div[contains(@class,"movieListOne")]')
+        for movie in movieList:
+            item = MoviesItem()
+            item.title = movie.xpath('h3/a/@title').extract()
+            item.link = movie.xpath('h3/a/@href').extract()
+            desc = movie.xpath('p/text()').extract()
+            item.actors = desc[0]
+            item.playtime = desc[len(desc)-1]
+            yield item
+
+            # pp = movie.xpath('/p').extract()
+            # log.msg("desc:%s" % (desc), level=log.DEBUG)
